21714dd010332945d88081c388e8cd1861f01eea
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 14:07:32 +0530

    Enhance Product schema with additional fields

--- models/Product.js
+++ models/Product.js
@@ -1,9 +1,35 @@
-
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number
-});
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+
+    folder: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Folder",
+    },
+
+    vendor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor",
+      required: true,
+    },
+
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+
+    subcategory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Subcategory",
+    },
+
+    image: String,
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Product", productSchema);
