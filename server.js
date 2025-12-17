b2af1f411a0ecd23eba70e5356597037b124174f
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 11:38:57 +0530

    Add subcategory routes to server

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import userOrderRoutes from "./routes/userOrderRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import folderRoutes from "./routes/folderRoutes.js";
 import vendorRoutes from "./routes/vendorRoutes.js";
+import subcategoryRoutes from "./routes/subcategoryRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -24,6 +25,7 @@ app.use("/api/user", userOrderRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/folders", folderRoutes);
 app.use("/api/vendors", vendorRoutes);
+app.use("/api/subcategories", subcategoryRoutes);
 
 app.listen(process.env.PORT, () =>
   console.log("Server running on", process.env.PORT)
