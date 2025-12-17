6e2fec8bbe7f36f4609dc18cd2141765ec97694c
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 08:20:33 +0530

    Add folder routes to server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import userOrderRoutes from "./routes/userOrderRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
+import folderRoutes from "./routes/folderRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -20,6 +21,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/user", userOrderRoutes);
 app.use("/api/categories", categoryRoutes);
+app.use("/api/folders", folderRoutes);
 
 app.listen(process.env.PORT, () =>
   console.log("Server running on", process.env.PORT)
