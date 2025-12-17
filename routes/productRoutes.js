7073593f998d49bd15be5cd9dfb36cf2c045adba
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 14:14:58 +0530

    Fix duplicate GET route in productRoutes
    Removed duplicate GET route for products.

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,15 +1,12 @@
 import express from "express";
 import {
-  createProduct,
-  getProducts
+  getProducts,
+  createProduct
 } from "../controllers/productController.js";
 
 const router = express.Router();
 
-// CREATE PRODUCT
-router.post("/", createProduct);
-
-// GET PRODUCTS (filters)
 router.get("/", getProducts);
+router.post("/", createProduct);
 
 export default router;
