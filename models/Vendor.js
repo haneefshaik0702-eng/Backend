3425829e47b5dbd5c527300e9ea8a4da907a6ad8
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 08:39:21 +0530

    Refactor vendorSchema definition in Vendor.js

--- models/Vendor.js
+++ models/Vendor.js
@@ -1,15 +1,8 @@
 import mongoose from "mongoose";
 
-const vendorSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    folder: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Folder",
-      required: true
-    }
-  },
-  { timestamps: true }
-);
+const vendorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  folder: { type: mongoose.Schema.Types.ObjectId, ref: "Folder", required: true },
+});
 
 export default mongoose.model("Vendor", vendorSchema);
