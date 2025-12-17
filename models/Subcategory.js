62bd81aca7e86e961c01ae10a31587400a8de7e2
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 11:38:01 +0530

    Create Subcategory model with schema definition

---
+++ models/Subcategory.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const subcategorySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Subcategory", subcategorySchema);
