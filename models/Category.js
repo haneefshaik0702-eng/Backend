9fa73e149a867978a19be40edd09f66e2f80a706
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 17:38:06 +0530

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -2,7 +2,15 @@ import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    name: {
+      type: String,
+      required: true
+    },
+    vendor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor",
+      required: true
+    }
   },
   { timestamps: true }
 );
