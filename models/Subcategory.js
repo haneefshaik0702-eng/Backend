9c4df3ad761dd68c51c365b21a64701905741ac2
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 17:56:49 +0530

    Update Subcategory.js

--- models/Subcategory.js
+++ models/Subcategory.js
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 const subcategorySchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    name: {
+      type: String,
+      required: true,
+    },
     category: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Category",
-      required: true
-    }
+      required: true,
+    },
   },
   { timestamps: true }
 );
