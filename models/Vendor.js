556746f031e9473960396b2443e7dc361d377ab5
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 08:22:42 +0530

    Create Vendor model with schema and export

---
+++ models/Vendor.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const vendorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  folder: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Folder",
+    required: true
+  }
+}, { timestamps: true });
+
+export default mongoose.model("Vendor", vendorSchema);
