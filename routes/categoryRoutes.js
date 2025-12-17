61cbf888d261664a48865eb93198177c9fd0ab80
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 09:04:33 +0530

    Implement category CRUD and subcategory retrieval
    Added admin CRUD operations for categories and user fetch for subcategories.

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { getCategories } from "../controllers/categoryController.js";
+import {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategories,
+  getSubCategoriesByCategory
+} from "../controllers/categoryController.js";
+import { protectAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+// Admin CRUD
+router.post("/", protectAdmin, createCategory);
+router.put("/:id", protectAdmin, updateCategory);
+router.delete("/:id", protectAdmin, deleteCategory);
+
+// User fetch
 router.get("/", getCategories);
+router.get("/:categoryId/subcategories", getSubCategoriesByCategory);
 
 export default router;
