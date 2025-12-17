e23a7b0a70366b060b2944525251e69a8277dd0d
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 11:28:23 +0530

    Add error handling to createCategory function
    Refactor createCategory to include error handling.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,33 +1,18 @@
 import Category from "../models/Category.js";
-import SubCategory from "../models/SubCategory.js";
 
-// Create category
 export const createCategory = async (req, res) => {
-  const category = await Category.create({ name: req.body.name, vendor: req.body.vendor });
-  res.status(201).json(category);
+  try {
+    const category = await Category.create({
+      name: req.body.name,
+      vendor: req.body.vendor,
+    });
+    res.status(201).json(category);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
-// Update category
-export const updateCategory = async (req, res) => {
-  const category = await Category.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
-  res.json(category);
-};
-
-// Delete category
-export const deleteCategory = async (req, res) => {
-  await SubCategory.deleteMany({ category: req.params.id });
-  const category = await Category.findByIdAndDelete(req.params.id);
-  res.json({ message: "Category deleted", category });
-};
-
-// Get all categories
 export const getCategories = async (req, res) => {
   const categories = await Category.find();
   res.json(categories);
-};
-
-// Get subcategories by category
-export const getSubCategoriesByCategory = async (req, res) => {
-  const subcategories = await SubCategory.find({ category: req.params.categoryId });
-  res.json(subcategories);
 };
