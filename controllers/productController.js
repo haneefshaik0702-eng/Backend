8c009c2cd13eda3d57a60c62fed9b12cfb12f28c
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 11:45:43 +0530

    Add createProduct function and refactor getProducts

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,33 @@
 import Product from "../models/Product.js";
 
+export const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create({
+      name: req.body.name,
+      price: req.body.price,
+      vendor: req.body.vendor,
+      category: req.body.category,
+      subcategory: req.body.subcategory,
+      image: req.body.image || ""
+    });
+
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to create product", error: err.message });
+  }
+};
+
 export const getProducts = async (req, res) => {
   try {
-    const { category } = req.query;
-
     const filter = {};
-    if (category) {
-      filter.category = category;
-    }
+
+    if (req.query.vendor) filter.vendor = req.query.vendor;
+    if (req.query.category) filter.category = req.query.category;
+    if (req.query.subcategory) filter.subcategory = req.query.subcategory;
 
     const products = await Product.find(filter);
     res.json(products);
-  } catch (err) {
+  } catch {
     res.status(500).json({ message: "Failed to load products" });
   }
 };
