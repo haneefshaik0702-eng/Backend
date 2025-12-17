7ae8c5bd64f3f909c1804240db22ac7fe3a53be7
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 08:26:50 +0530

    Refactor vendor routes to use controller functions

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -1,23 +1,9 @@
 import express from "express";
-import Vendor from "../models/Vendor.js";
+import { createVendor, getVendors } from "../controllers/vendorController.js";
 
 const router = express.Router();
 
-// Create vendor (admin)
-router.post("/", async (req, res) => {
-  try {
-    const vendor = await Vendor.create(req.body);
-    res.json(vendor);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// Get vendors by folder (user)
-router.get("/", async (req, res) => {
-  const { folder } = req.query;
-  const vendors = await Vendor.find({ folder });
-  res.json(vendors);
-});
+router.post("/", createVendor);
+router.get("/", getVendors);
 
 export default router;
