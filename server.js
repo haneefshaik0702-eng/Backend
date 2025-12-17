edea82c37f6d51f0f40e428c910d7877fe02dc17
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 08:41:27 +0530

    Add vendor routes to server

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import productRoutes from "./routes/productRoutes.js";
 import userOrderRoutes from "./routes/userOrderRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import folderRoutes from "./routes/folderRoutes.js";
+import vendorRoutes from "./routes/vendorRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -22,6 +23,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/user", userOrderRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/folders", folderRoutes);
+app.use("/api/vendors", vendorRoutes);
 
 app.listen(process.env.PORT, () =>
   console.log("Server running on", process.env.PORT)
