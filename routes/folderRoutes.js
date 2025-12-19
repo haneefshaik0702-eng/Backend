d7834e1ff33a59fa8009a3ef2fff3c1d49552d42
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-19 11:03:53 +0530

    Update folderRoutes.js

--- routes/folderRoutes.js
+++ routes/folderRoutes.js
@@ -1,22 +1,8 @@
 import express from "express";
-import Folder from "../models/Folder.js";
+import { getFolders } from "../controllers/folderController.js";
 
 const router = express.Router();
 
-// Create folder (admin)
-router.post("/", async (req, res) => {
-  try {
-    const folder = await Folder.create(req.body);
-    res.json(folder);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// Get all folders (user)
-router.get("/", async (req, res) => {
-  const folders = await Folder.find();
-  res.json(folders);
-});
+router.get("/", getFolders);
 
 export default router;
