08ba7f9561f4932c9b44f988f24835842a2b2777
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-19 14:22:14 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,33 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String },
-  image: { type: String },
-  vendor: { type: mongoose.Schema.Types.ObjectId, ref: "Vendor", required: true },
-  subcategory: { type: mongoose.Schema.Types.ObjectId, ref: "SubCategory", required: true },
-});
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    vendor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor",
+      required: true,
+    },
+    subcategory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "SubCategory",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Product", productSchema);
