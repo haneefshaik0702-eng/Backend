bfd08493e30e995dab4a81625b2d2666854dcdf6
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-19 10:18:19 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,9 @@
+import express from "express";
 import { login, register } from "../controllers/authController.js";
 
+const router = express.Router();
+
 router.post("/login", login);
-router.post("/register", register);  // ← add this
+router.post("/register", register);  // make sure this line exists
+
+export default router;
