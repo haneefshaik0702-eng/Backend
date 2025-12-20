3185eefed9fdef13d4537deb3897b2ba5623fd44
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-20 15:52:04 +0530

    Update subcategoryRoutes.js

--- routes/subcategoryRoutes.js
+++ routes/subcategoryRoutes.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { getSubcategories } from "../controllers/subcategoryController.js";
+import Subcategory from "../models/Subcategory.js";
+import { protectAdmin } from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", getSubcategories); // ?category=CATEGORY_ID
+router.post("/", protectAdmin, async (req, res) => {
+  const { name, category } = req.body;
+  const subcategory = new Subcategory({ name, category });
+  await subcategory.save();
+  res.json(subcategory);
+});
+
+router.get("/", async (req, res) => {
+  const { category } = req.query;
+  const filter = category ? { category } : {};
+  const subcategories = await Subcategory.find(filter);
+  res.json(subcategories);
+});
 
 export default router;
